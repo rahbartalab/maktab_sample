a682b14d7bb042f1b6dc229dc71aa9999900af33
hossein <rahbartalab2002@gmail.com>
2024-03-07 15:04:19 +0330

    [feat] config tailwind css & login check functionality

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,13 +2,28 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\SubmitLoginRequest;
 use App\Models\User;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class AuthController extends Controller
 {
     public function showLogin()
     {
         return view('auth.login');
+    }
+
+    public function submitLogin(SubmitLoginRequest $request): RedirectResponse
+    {
+        if (Auth::attempt($request->validated())) {
+            $request->session()->regenerate();
+            return redirect()->route('posts.index');
+        }
+
+        return back()->withErrors([
+            'email' => 'The provided credentials do not match our records.',
+        ])->onlyInput('email');
     }
 }
--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -5,19 +5,26 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
+    @vite('resources/css/app.css')
 </head>
-<body>
+<body class="bg-slate-200">
 <h2>Login</h2>
 
 <form action="{{ route('login.check') }}" method="post">
     @csrf
     <label for="email">email</label>
     <input type="text" id="email" name="email">
-
+    @error('email')
+    <p class="text-red-600">{{ $message }}</p>
+    @enderror
+    <br>
     <label for="password">password</label>
-    <input type="text" id="password" name="password">
+    <input type="password" id="password" name="password">
+    @error('password')
+    <p class="text-red-600">{{ $message }}</p>
+    @enderror
 
-    <input type="submit" name="login">
+    <input class="py-1 px-2 rounded-xl bg-slate-700 text-white" type="submit" name="login">
 </form>
 
 
